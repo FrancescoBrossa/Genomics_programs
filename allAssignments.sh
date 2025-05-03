d6cc8067084ad1550660944a8158650f041f75cc
FrancescoBrossa <brossa.001@gmail.com>
2025-05-03 17:46:48 +0200

    Update allAssignments.sh

--- allAssignments.sh
+++ allAssignments.sh
@@ -4,9 +4,7 @@
 # for all the 10 cases
 # the file needs a text file named ourcases.txt
 
-# the for take each line of the file
-#for line in $(cat "ourcases.txt"); do
-# needed this while loop, it seems
+
 while IFS= read -r line; do
 	#fields=($(echo "$line" | cut -d ";" -f 1) $(echo "$line" | cut -d ";" -f 2))
 	field1=$(echo "$line" | cut -d ";" -f 1)
