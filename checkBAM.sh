94a3084659721af305b9c844177ad176a8cb5d25
FrancescoBrossa <brossa.001@gmail.com>
2025-05-03 17:50:22 +0200

    Update checkBAM.sh

--- checkBAM.sh
+++ checkBAM.sh
@@ -5,10 +5,10 @@ if ! [ $# -eq 1 ]; then
 	echo "wrong number of argument for checkBAM.sh"
 	exit 1
 fi
-# take as argument a folder number check
-# 1. if the folder exists
+# take as argument a folder number 
+# 1. check if the folder exists
 [ -d "case$1" ] || ./importcases.sh $1
-# 2, if it exists check if it there are BAM file inside
+# check if the command worked
 if ! [ -d "case$1" ]; then
 	echo "importcases.sh didn't work"
 	exit 1
@@ -18,8 +18,10 @@ first_d=$(pwd)
 cd case$1
 pattern=(*.bam)
 echo "${#pattern[@]}"
+# 2. if the folder exists check if there are 3 .bam file inside
 if ! [ ${#pattern[@]} -eq 3 ]; then
         echo "there are not 3 bam files inside the folder"
+	# try to make .bam files with mapFQfiles.sh
         ../mapFQfiles.sh $1
 fi
 # now check if mapFQfiles.sh worked
